25c163b8c01ddb471eb87d2d1b156b273105ef36
ftnext <takuyafjp+develop@gmail.com>
2024-09-22 01:29:46 +0900

    [feat] Parse single line dependencies

---
+++ src/pep723/parser.py
@@ -0,0 +1,24 @@
+import re
+import tomllib
+from typing import TypedDict, cast
+
+# ref: https://peps.python.org/pep-0723/#specification
+REGEX = (
+    r"(?m)^# /// (?P<type>[a-zA-Z0-9-]+)$\s(?P<content>(^#(| .*)$\s)+)^# ///$"
+)
+
+
+class ScriptMetadata(TypedDict):
+    dependencies: list[str]
+
+
+def parse(script: str) -> ScriptMetadata:
+    name = "script"
+    matches = list(
+        filter(lambda m: m.group("type") == name, re.finditer(REGEX, script))
+    )
+    content = "".join(
+        line[2:]
+        for line in matches[0].group("content").splitlines(keepends=True)
+    )
+    return cast(ScriptMetadata, tomllib.loads(content))
