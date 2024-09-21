630a34ea4a5771583e5b36c08681e19fe7a4764a
ftnext <takuyafjp+develop@gmail.com>
2024-09-22 02:03:59 +0900

    [feat] Crude support inline dependencies
    python -m pep723.tool example.py

--- src/pep723/tool/__main__.py
+++ src/pep723/tool/__main__.py
@@ -1,1 +1,12 @@
-print("Hello PEP 723!")
+import tempfile
+
+from pep723.parser import parse
+from pep723.tool.cli import parse_args
+from pep723.tool.runner import run
+from pep723.tool.venv import create_venv_with_dependencies
+
+args = parse_args()
+metadata = parse(args.script.read_text())
+with tempfile.TemporaryDirectory() as tmpdir:
+    create_venv_with_dependencies(tmpdir, metadata["dependencies"])
+    run(f"{tmpdir}/bin/python", str(args.script))
