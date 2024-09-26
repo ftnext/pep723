9053a66973c1d6f074627b157ef02db954e70d79
ftnext <takuyafjp+develop@gmail.com>
2024-09-26 22:57:11 +0900

    [docs] Example script

---
+++ example/script_with_metadata.py
@@ -0,0 +1,13 @@
+# /// script
+# dependencies = [
+#   "httpx",
+#   "rich",
+# ]
+# ///
+
+import httpx
+from rich.pretty import pprint
+
+resp = httpx.get("https://peps.python.org/api/peps.json")
+data = resp.json()
+pprint([(k, v["title"]) for k, v in data.items()][:5])
